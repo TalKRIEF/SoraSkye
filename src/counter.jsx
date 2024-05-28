82db352017c45d38f20db178372a1144cc51d911
Tal KRIEF <talkrief@gmail.com>
2024-05-28 14:25:59 +0200

    ?

--- src/counter.jsx
+++ src/counter.jsx
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 
+
 function Counter() {
 
   const [count, setCount] = useState(0)
+  const first_name = "Teva"
+  const last_name = "Griegou"
   return (
     <div>
       <p>Tu as cliqué {count} fois !</p>
       <button onClick={() => setCount(count +1)}>Clique sur ma gueule !</button>
       <button onClick={() => setCount(0)}> Reset le count </button>
+      <p> Logged in as {first_name} {last_name} </p>
     </div>
   );
 }
