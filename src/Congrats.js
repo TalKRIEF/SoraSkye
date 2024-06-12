63c7929ab881423ab8c07a2f8bdcde4e87e75e84
Tal KRIEF <talkrief@gmail.com>
2024-06-12 21:00:14 +0200

    fixed bug pixel blanc sur hover

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,15 +1,7 @@
 import React from 'react';
-// import generateCards from './generateCards';
 
 function Congrats({ matchedCards, totalCards }) {
   const isGameOver = matchedCards.length === totalCards;
-
-  // const resetGame = () => {
-  //   generateCards();
-  //   setFlippedCards([]);
-  //   setMatchedCards([]);
-  // };
-
   return (
     isGameOver && (
         <p>Félicitations ! Vous avez trouvé toutes les paires !</p>
